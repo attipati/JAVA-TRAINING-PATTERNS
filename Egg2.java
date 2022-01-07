c035c71f6384047507a9cf321427ddc2c3fc6fd8
attipati <96856522+attipati@users.noreply.github.com>
2022-01-07 18:46:17 +0530

    Add files via upload

---
+++ Egg2.java
@@ -0,0 +1,53 @@
+package Patterns;
+
+//import java.util.Scanner;
+
+public class Egg2 {
+    public static void main(String[] args) {
+        printE1();
+        printE2();
+        printE3();
+        printE4();
+        
+    }
+    public static void printE1(){
+        System.out.println("    __________");
+        System.out.println("   /          \\");
+        System.out.println("  /            \\");
+        System.out.println("  \\            /");
+        System.out.println( "   \\__________/");
+        System.out.println();
+        System.out.println();
+        System.out.println("   -'-\"-'-\"-'- ");
+        
+    }
+    public static void printE2(){
+        System.out.println("     _________ ");
+        System.out.println("   /          \\");
+        System.out.println("  /            \\");
+        System.out.println("  \\            /");
+        System.out.println( "   \\__________/");
+        System.out.println();
+        System.out.println();
+        
+    }
+    public static void printE3(){
+        System.out.println("   -\"-'-\"-'-\"-");
+        System.out.println("  \\            /");
+        System.out.println( "   \\__________/");
+        System.out.println();
+        System.out.println();
+    }
+    public static void printE4(){
+        System.out.println("     _________ ");
+        System.out.println("   /          \\");
+        System.out.println("  /            \\");
+        System.out.println("  -\"-'-\"-'-\"-");
+        System.out.println("  \\            /");
+        System.out.println( "   \\__________/");
+        System.out.println();
+        System.out.println();
+    }
+    
+    
+}
