e5491e4b11ffdd3181bc4a175d51fadb0e7270a7
attipati <96856522+attipati@users.noreply.github.com>
2022-01-10 09:54:52 +0530

    Add files via upload

---
+++ TwoRockets.java
@@ -0,0 +1,43 @@
+package Patterns;
+
+public class TwoRockets {
+    public static void main(String[] args)
+    {
+        printTriangle1();
+        printSquare1();
+        printSquare2();
+        printTraiangle2();
+        
+    }
+    public static void printTriangle1()
+    {
+        System.out.println("       /\\               /\\    ");
+        System.out.println("      /  \\             /  \\   ");
+        System.out.println("     /    \\           /    \\  ");
+    }
+    public static void printSquare1()
+    {
+        System.out.println("   +--------+       +---------+");
+        System.out.println("   |         |      |          |");
+        System.out.println("   |         |      |          |");
+        System.out.println("   +---------+      +----------+");
+    }
+    public static void printSquare2()
+    {
+        System.out.println("   |united    |     |united    |");
+        System.out.println("   |states    |     |states    |");
+        System.out.println("   +---------+      +----------+");
+        System.out.println("   +---------+      +---------+");
+        System.out.println("   |         |      |          |");
+        System.out.println("   |         |      |          |");
+        System.out.println("   +---------+      +----------+");
+    }
+    public static void printTraiangle2()
+    {
+        System.out.println("       /\\               /\\    ");
+        System.out.println("      /  \\             /  \\   ");
+        System.out.println("     /    \\           /    \\  ");
+    }
+}
+
+   
